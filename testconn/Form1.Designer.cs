bd0b479e1f39ebcc259c02845438af16deb8d29e
洋 吴 <1936793928@qq.com>
2021-08-24 16:14:55 +0800

    上传源码

---
+++ testconn/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace testconn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTestConn = new System.Windows.Forms.Button();
+            this.lblIp = new System.Windows.Forms.Label();
+            this.txtIp = new System.Windows.Forms.TextBox();
+            this.txtDataBase = new System.Windows.Forms.TextBox();
+            this.lblDataBase = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserPwd = new System.Windows.Forms.TextBox();
+            this.lblUserPwd = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnTestConn
+            // 
+            this.btnTestConn.Location = new System.Drawing.Point(249, 266);
+            this.btnTestConn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnTestConn.Name = "btnTestConn";
+            this.btnTestConn.Size = new System.Drawing.Size(127, 44);
+            this.btnTestConn.TabIndex = 0;
+            this.btnTestConn.Text = "测试连接";
+            this.btnTestConn.UseVisualStyleBackColor = true;
+            this.btnTestConn.Click += new System.EventHandler(this.btnTestConn_Click);
+            // 
+            // lblIp
+            // 
+            this.lblIp.AutoSize = true;
+            this.lblIp.Location = new System.Drawing.Point(191, 69);
+            this.lblIp.Name = "lblIp";
+            this.lblIp.Size = new System.Drawing.Size(90, 15);
+            this.lblIp.TabIndex = 1;
+            this.lblIp.Text = "服务器地址:";
+            // 
+            // txtIp
+            // 
+            this.txtIp.Location = new System.Drawing.Point(294, 64);
+            this.txtIp.Name = "txtIp";
+            this.txtIp.Size = new System.Drawing.Size(144, 25);
+            this.txtIp.TabIndex = 2;
+            // 
+            // txtDataBase
+            // 
+            this.txtDataBase.Location = new System.Drawing.Point(294, 110);
+            this.txtDataBase.Name = "txtDataBase";
+            this.txtDataBase.Size = new System.Drawing.Size(144, 25);
+            this.txtDataBase.TabIndex = 4;
+            // 
+            // lblDataBase
+            // 
+            this.lblDataBase.AutoSize = true;
+            this.lblDataBase.Location = new System.Drawing.Point(191, 115);
+            this.lblDataBase.Name = "lblDataBase";
+            this.lblDataBase.Size = new System.Drawing.Size(97, 15);
+            this.lblDataBase.TabIndex = 3;
+            this.lblDataBase.Text = "数据库名称：";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(294, 152);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(144, 25);
+            this.txtUserName.TabIndex = 6;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Location = new System.Drawing.Point(191, 157);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(75, 15);
+            this.lblUserName.TabIndex = 5;
+            this.lblUserName.Text = "登录账号:";
+            // 
+            // txtUserPwd
+            // 
+            this.txtUserPwd.Location = new System.Drawing.Point(294, 195);
+            this.txtUserPwd.Name = "txtUserPwd";
+            this.txtUserPwd.Size = new System.Drawing.Size(144, 25);
+            this.txtUserPwd.TabIndex = 8;
+            // 
+            // lblUserPwd
+            // 
+            this.lblUserPwd.AutoSize = true;
+            this.lblUserPwd.Location = new System.Drawing.Point(191, 200);
+            this.lblUserPwd.Name = "lblUserPwd";
+            this.lblUserPwd.Size = new System.Drawing.Size(75, 15);
+            this.lblUserPwd.TabIndex = 7;
+            this.lblUserPwd.Text = "登录密码:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(711, 375);
+            this.Controls.Add(this.txtUserPwd);
+            this.Controls.Add(this.lblUserPwd);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtDataBase);
+            this.Controls.Add(this.lblDataBase);
+            this.Controls.Add(this.txtIp);
+            this.Controls.Add(this.lblIp);
+            this.Controls.Add(this.btnTestConn);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "测试数据库链接";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTestConn;
+        private System.Windows.Forms.Label lblIp;
+        private System.Windows.Forms.TextBox txtIp;
+        private System.Windows.Forms.TextBox txtDataBase;
+        private System.Windows.Forms.Label lblDataBase;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtUserPwd;
+        private System.Windows.Forms.Label lblUserPwd;
+    }
+}
+
---
+++ testconn/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testconn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// 数据库链接字符串
+        /// demo: string dbConn = "Data Source=127.0.0.1;Initial Catalog=OrBitXIScadaDB;Persist Security Info=True;User ID=sa;Password=123456";
+
+
+
+        /// <summary>
+        /// 测试连接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnTestConn_Click(object sender, EventArgs e)
+        {
+
+            if (txtDataBase.Text == "" || txtIp.Text == "" || txtUserName.Text == "" || txtUserPwd.Text == "")
+            {
+                MessageBox.Show("请输入正确的信息！");
+                return;
+            }
+
+            string dbConn = "Data Source={0};Initial Catalog={1};Persist Security Info=True;User ID={2};Password={3}";
+
+            dbConn = string.Format(dbConn, txtIp.Text, txtDataBase.Text, txtUserName.Text, txtUserPwd.Text);
+
+            SqlConnection m_SqlConn = new SqlConnection(dbConn);
+
+            try
+            {
+                m_SqlConn.Open();
+
+                if (m_SqlConn.State == ConnectionState.Open)
+                {
+                    MessageBox.Show("连接成功");
+                }
+                else
+                {
+
+                    MessageBox.Show("连接失败"+ "  "+ m_SqlConn.State.ToString());
+                }    
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return;
+            }
+
+        }
+
+        /// <summary>
+        /// 记录日志
+        /// </summary>
+        /// <param name="log"></param>
+        public void LogError(string log)
+        {
+            string sFilePath = "D:\\Logs\\" + DateTime.Now.ToString("yyyyMM");
+            string sFileName = "log_" + DateTime.Now.ToString("dd") + ".log";
+            sFileName = sFilePath + "\\" + sFileName; //文件的绝对路径
+            if (!Directory.Exists(sFilePath))//验证路径是否存在
+            {
+                Directory.CreateDirectory(sFilePath);
+                //不存在则创建
+            }
+            FileStream fs;
+            StreamWriter sw;
+            if (File.Exists(sFileName))
+            //验证文件是否存在，有则追加，无则创建
+            {
+                fs = new FileStream(sFileName, FileMode.Append, FileAccess.Write);
+            }
+            else
+            {
+                fs = new FileStream(sFileName, FileMode.Create, FileAccess.Write);
+            }
+            sw = new StreamWriter(fs);
+            sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss") + "   ---   " + log);
+            sw.Close();
+            fs.Close();
+
+        }
+
+    }
+}
